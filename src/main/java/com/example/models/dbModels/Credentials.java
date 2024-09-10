8e45db068ff4459b2d0571dd0c96cfcc1a360be4
naveen <naveengummula44@gmail.com>
2024-09-10 15:51:00 +0530

    Ecom App implemented with THymleaf files

---
+++ src/main/java/com/example/models/dbModels/Credentials.java
@@ -0,0 +1,17 @@
+package com.example.models.dbModels;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Credentials
+{
+    @Id
+    @SequenceGenerator(name = "seqGen44",sequenceName = "CredentialsSeq", initialValue = 1500, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqGen44")
+    private Integer id;
+    private String userName;
+    private String password;
+    private String roles;
+}
